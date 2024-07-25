9bc851de48b4ebe012dcf413b4490100db6afcd9
RossJames-lab <ross@newlandshealthcare.co.uk>
2024-07-25 12:17:57 +0700

    created accordian structure in index.jsx

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,3 +1,25 @@
+import React, { useState } from 'react';
+import data from "./data";
+
 export default function Accordian() {
-  return <div>Accordian</div>;
+  const [selected, setSeleted] = useState(null);
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}> {/* Assuming dataItem has a unique id */}
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
 }
