f6ddb57dacb94c52d1875f62095566b0586dcd9a
RossJames-lab <ross@newlandshealthcare.co.uk>
2024-07-25 11:55:35 +0700

    Created Accordian Component

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      id: '1',
+      question: "What is an accordion?",
+      answer: "An accordion is a portable musical instrument that is played by compressing and expanding a bellows while pressing buttons or keys to open valves and allow air to flow across metal reeds."
+    },
+    {
+      id: '2',
+      question: "How does an accordion produce sound?",
+      answer: "An accordion produces sound by forcing air through reeds that vibrate to create musical tones. The bellows' movement controls the flow of air and the buttons or keys open and close the reeds."
+    },
+    {
+      id: '3',
+      "question": "What are the main types of accordions?",
+      "answer": "The main types of accordions include the diatonic accordion, which is tuned to a specific key, and the chromatic accordion, which is tuned to play all the notes in a chromatic scale. There are also variations like the piano accordion and the button accordion."
+    },
+    {
+      id: '4',
+      question: "What genres of music commonly use the accordion?",
+      answer: "The accordion is commonly used in various genres including folk music, polka, tango, and classical music. It is particularly popular in European folk traditions and in Latin American music styles."
+    }
+  ];
+  
+  export default data
